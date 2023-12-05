5894b8edfa89ef375dec7b2d36a6d78903a5c519
waisalkindy <waisalkindy@gmail.com>
2023-12-05 18:00:00 +0100

    making prediction

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -11,6 +11,8 @@ from datetime import datetime
 import os
 from django.conf import settings
 from .utils import save_plot
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import load_model
 
 
 
@@ -77,6 +79,39 @@ class StockPredictionAPIView(APIView):
             # Save the plot to a file
             plot_img_path = f"{ticker}_200_DMA.png"
             plot_200_dma = save_plot(plot_img_path)
+
+
+            # Splitting data into Training & Testing datasets
+            data_training = pd.DataFrame(df.Close[0:int(len(df)*0.7)])
+            data_testing = pd.DataFrame(df.Close[int(len(df)*0.7): int(len(df))])
+
+            # Scalling down the data between 0 and 1
+            scaler = MinMaxScaler(feature_range=(0,1))
+
+            # Load ML Model
+            model = load_model('stock_prediction_model.keras')
+
+            # Preparing test data
+            past_100_days = data_training.tail(100)
+            final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
+            input_data = scaler.fit_transform(final_df)
+
+            x_test = []
+            y_test = []
+
+            for i in range(100, int(input_data.shape[0])):
+                x_test.append(input_data[i-100:i])
+                y_test.append(input_data[i, 0])
+            x_test, y_test = np.array(x_test), np.array(y_test)
+
+            # Making Predictions
+            y_predicted = model.predict(x_test)
+
+            # Revert the scaled prices to original prices
+            y_predicted = scaler.inverse_transform(y_predicted.reshape(-1,1)).flatten()
+            y_test = scaler.inverse_transform(y_test.reshape(-1,1)).flatten()
+
+            # Plot the final prediction
 
 
             return Response({'status': 'success',
