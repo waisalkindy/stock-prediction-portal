7909c11c0e561e9976fb651076920791ceb12149
waisalkindy <waisalkindy@gmail.com>
2024-06-17 18:00:00 +0100

    serializers views an urls

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from .serializers import StockPredictionSerializer
+from rest_framework import status
+from rest_framework.response import Response
 # Create your views here.
+
+
+class StockPredictionAPIView(APIView):
+    def post(self, request):
+        serializer = StockPredictionSerializer(data=request.data)
+        if serializer.is_valid():
+            ticker = serializer._validated_data['ticker']
+            return Response({'status': 'success'})
