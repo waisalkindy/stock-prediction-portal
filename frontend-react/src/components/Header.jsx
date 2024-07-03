8a7ba3bb45055b4736d34936f593dd18cf4f5037
waisalkindy <waisalkindy@gmail.com>
2024-07-03 09:00:00 +0100

    Header design

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='text-light'>Header</div>
+    <>
+      <nav className='navbar container pt-3 pb-3'>
+        <a className='navbar-brand text-light' href="">Stock Prediction Portal</a>
+
+        <div>
+          <a className='btn btn-outline-info' href="">Login</a>
+          &nbsp;
+          <a className='btn btn-info' href="">Register</a>
+        </div>
+      </nav>
+    </>
   )
 }
 
