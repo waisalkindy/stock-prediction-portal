ffa24d27c9616893198a65970a1cdabef333e453
waisalkindy <waisalkindy@gmail.com>
2024-07-04 09:00:00 +0100

    main component design

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div className='text-light'>Main</div>
+    <>
+      <div className='container'>
+        <div className='p-5 text-center bg-light-dark rounded'>
+          <h1 className='text-light'>Stock Prediction App</h1>
+          <p className='text-light lead'>The stock prediction application utilizes machine learning techniques, specifically employing Keras, and LSTM model, integrated within the Django framework. It forecasts future stock prices by analyzing 100-day and 200-day moving averages, essential indicators widely used by stock analysts to inform trading and investment decisions.</p>
+          <a className='btn btn-info' href="">Login</a>
+        </div>
+      </div>
+    </>
   )
 }
 
