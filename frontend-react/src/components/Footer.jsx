23585c220da5e47407137ec1cf511d9a250574cf
waisalkindy <waisalkindy@gmail.com>
2024-07-05 09:00:00 +0100

    footer component

--- frontend-react/src/components/Footer.jsx
+++ frontend-react/src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-light'>Footer</div>
+    <>
+      <footer className='footer py-3 my-3'>
+      <hr className='border-bottom' />
+      <p className='text-light text-center'>&copy; 2024 - Build with ❤️ by Wais Alkindy</p>
+      </footer>
+    </>
   )
 }
 
