f86210df3121b2619f04dc3774a46682f10017b8
waisalkindy <waisalkindy@gmail.com>
2024-07-18 09:00:00 +0100

    design register page

--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div className='text-light'>Register</div>
+    <>
+    <div className='container'>
+      <div className='row justify-content-center'>
+        <div className='col-md-6 bg-light-dark p-5 rounded'>
+          <h3 className='text-light text-center mb-4'>Create an account</h3>
+          <form>
+            <input type="text" className='form-control mb-3' placeholder='Username'/>
+            <input type="email" className='form-control mb-3' placeholder='Email address' />
+            <input type="password" className='form-control mb-5' placeholder='Set password' />
+            <button type='submit' className='btn btn-info d-block mx-auto'>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
