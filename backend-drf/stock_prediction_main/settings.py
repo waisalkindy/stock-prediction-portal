cfdbc7cb9beeba58d623d7e26e0574ea4e609a5c
waisalkindy <waisalkindy@gmail.com>
2024-08-22 18:00:00 +0100

    using response interceptor for refresh token

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 
 }
--- frontend-react/src/axiosInstance.js
+++ frontend-react/src/axiosInstance.js
@@ -10,18 +10,44 @@ const axiosInstance = axios.create({
 })
 
 
-// Request Interceptors
+// Request Interceptor
 axiosInstance.interceptors.request.use(function(config){
   const accessToken = localStorage.getItem('accessToken')
   if(accessToken){
     config.headers['Authorization'] = `Bearer ${accessToken}`
   }
-  console.log(config);
   return config;
-}, function(error){
+},
+// Handle failed request
+function(error){
   return Promise.reject(error);
-}
-)
+})
+
+// Response Interceptor
+axiosInstance.interceptors.response.use(function(response){
+  return response;
+},
+// Handle failed response
+async function(error){
+  const originalRequest = error.config;
+  if(error.response.status === 401 && !originalRequest.retry){
+    originalRequest.retry = true;
+    const refreshToken = localStorage.getItem('refreshToken')
+    try{
+      const response = await axiosInstance.post('/token/refresh/', {refresh: refreshToken})
+      console.log('New Access Token', response.data.access)
+      localStorage.setItem('accessToken', response.data.access)
+      originalRequest.headers['Authorization'] = `Bearer ${response.data.access}`
+      return axiosInstance(originalRequest)
+    }catch(error){
+      localStorage.removeItem('accessToken')
+      localStorage.removeItem('refreshToken')
+      window.location.href = '/login'
+    }
+  }
+  return Promise.reject(error);
+})
+
 
 
 export default axiosInstance;
