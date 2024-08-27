0efdd4b1a199bace19e6419bcf839c0178d01e1b
waisalkindy <waisalkindy@gmail.com>
2024-08-27 18:00:00 +0100

    handle public route

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -9,6 +9,7 @@ import Login from './components/Login'
 import AuthProvider from './AuthProvider'
 import Dashboard from './components/dashboard/Dashboard'
 import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
 
 
 function App() {
@@ -20,8 +21,8 @@ function App() {
       <Header />
         <Routes>
           <Route path='/' element={<Main />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<PublicRoute><Register /></PublicRoute>} />
+          <Route path='/login' element={<PublicRoute><Login /></PublicRoute>} />
           <Route path='/dashboard' element={<PrivateRoute><Dashboard /></PrivateRoute>} />
         </Routes>
       <Footer />
--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
       localStorage.setItem('refreshToken', response.data.refresh)
       console.log('Login successful');
       setIsLoggedIn(true);
-      navigate('/')
+      navigate('/dashboard')
     }catch(error){
       console.log('invalid credential')
       setError('Invalid Credential')
